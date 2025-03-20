b3f8078a7d8070eb2f7b3d7b19b501f9abe232ff
WendyWong083 <wendywongrthk@gmail.com>
2025-03-20 22:53:51 +0800

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "wendywongrthk@gmail.com"; // Your email address
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Unable to send your message. Please try again.";
+    }
+}
+?>
